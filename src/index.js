689bd1cef45046dec17e01b5640f54095a5969c3
Kahaan <kahaan.r.patel@gmail.com>
2018-12-14 10:43:19 -0800

    adding db

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-const { GraphQLServer } = require('graphql-yoga')
+const {GraphQLServer} = require('graphql-yoga');
 
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
 
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
+    // link (where: id!): Link,
+    // link: (root, {id}) => {
+    //   let selectedLink = links.filter(link => {
+    //     console.log(`******${id}`);
+    //     console.log(`******${link.id}`);
+    //     link.id === id;
+    //   });
+    //   console.log(selectedLink);
+    //   return selectedLink
+    // },
 
-    link: (id) => links.filter((link) => link.id === id ),
   },
-
 
   Mutation: {
     post: (root, args) => {
@@ -25,33 +33,28 @@ const resolvers = {
         id: `link-${idCount++}`,
         description: args.description,
         url: args.url,
-      }
-      links.push(link)
-      return link
+      };
+      links.push(link);
+      return link;
     },
 
-    updateLink: (root, args) => {
-
-      let link = links.filter((link) => link.id === args.id ? link : null)
-
-       link = {
-        id: args.id,
-        url: args.url,
-        description: args.description,
-      }
-
-      return link
-    }
-
-  
+    // updateLink: (root, args) => {
+    //
+    //   let link = links.filter((link) => link.id === args.id ? link : null)
+    //
+    //    link = {
+    //     id: args.id,
+    //     url: args.url,
+    //     description: args.description,
+    //   }
+    //
+    //   return link
+    // }
   },
-
-
-}
-
+};
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
